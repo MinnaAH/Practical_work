8ee0b506cf7b45c24c5d11ed1eb09eedcd6515fd
Minna <m.hannula@elisanet.fi>
2018-01-14 13:14:58 +0200

    Finished

--- Rekisteri/Reskisteri/Henkilo.cpp
+++ Rekisteri/Reskisteri/Henkilo.cpp
@@ -1,68 +1,108 @@
 #include "Henkilo.h"
 #include <iostream>
-using std::endl; using std::cout; using std::cin; using std::getline;
+#include <fstream>
+using std::endl; using std::cout; using std::cin; using std::getline; using std::ofstream;
 
+/*------------------------------------------------
+*name: Henkilo()
+*action: defaul constructor
+-------------------------------------------------*/
 Henkilo::Henkilo() :etunimi_(), sukunimi_(), osoite_(), puhelinnumero_()
 {
 }
-
+/*----------------------------------------------------------------------------------
+*name: Henkilo(string etunimi, string sukunimi, string osoite, string puhelinnumero)
+*action: Parameter constructor
+-----------------------------------------------------------------------------------*/
 Henkilo::Henkilo(string etunimi, string sukunimi, string osoite,
 	string puhelinnumero) : etunimi_(etunimi), sukunimi_(sukunimi),
 	osoite_(osoite), puhelinnumero_(puhelinnumero)
 {
 }
-
+/*------------------------------------------------
+*name: Henkilo(const Henkilo &alkup)
+*action: copy constructor
+-------------------------------------------------*/
 Henkilo::Henkilo(const Henkilo & alkup) : etunimi_(alkup.etunimi_),
 sukunimi_(alkup.sukunimi_), osoite_(alkup.osoite_),
 puhelinnumero_(alkup.puhelinnumero_)
 {
 }
-
-
+/*------------------------------------------------
+*name: ~Henkilo()
+*action: destructor
+-------------------------------------------------*/
 Henkilo::~Henkilo()
 {
 }
-
+/*----------------------------------------------------------------
+*name: annaEtunimi() const
+*action: Get firstname
+-----------------------------------------------------------------*/
 string Henkilo::annaEtunimi() const
 {
 	return etunimi_;
 }
-
+/*-----------------------------------------------------------------
+*name: annaSukunimi() const
+*action: Get lastname
+-------------------------------------------------------------------*/
 string Henkilo::annaSukunimi() const
 {
 	return sukunimi_;
 }
-
+/*----------------------------------------------------------------
+*name: annaOsoite() const
+*action: Get address
+-----------------------------------------------------------------*/
 string Henkilo::annaOsoite() const
 {
 	return osoite_;
 }
-
+/*----------------------------------------------------------------------
+*name: annaPuhelinnumero() const
+*action: Get phone number
+-----------------------------------------------------------------------*/
 string Henkilo::annaPuhelinnumero() const
 {
 	return puhelinnumero_;
 }
-
+/*-----------------------------------------------------------------------
+*name: asetaEtunimi(string etunimi)
+*action: Set firstname
+-------------------------------------------------------------------------*/
 void Henkilo::asetaEtunimi(string etunimi)
 {
 	etunimi_ = etunimi;
 }
-
+/*------------------------------------------------------------------------
+*name: asetaSukunimi(strgin sukunimi)
+*action: set lastname
+-------------------------------------------------------------------------*/
 void Henkilo::asetaSukunimi(string sukunimi)
 {
 	sukunimi_ = sukunimi;
 }
-
+/*------------------------------------------------------------------------
+*name: asetaOsoite(string osoite)
+*action: Set address
+-------------------------------------------------------------------------*/
 void Henkilo::asetaOsoite(string osoite)
 {
 	osoite_ = osoite;
 }
-
+/*------------------------------------------------------------------------
+*name: asetaPuhelinnumero(string puhelinnumero)
+*action: Set phone number
+--------------------------------------------------------------------------*/
 void Henkilo::asetaPuhelinnumero(string puhelinnumero)
 {
 	puhelinnumero_ = puhelinnumero;
 }
-
+/*------------------------------------------------
+*name: kysyTiedot()
+*action: Ask person information from user
+-------------------------------------------------*/
 void Henkilo::kysyTiedot()
 {
 	cout << "Anna etunimi: ";
@@ -74,9 +114,31 @@ void Henkilo::kysyTiedot()
 	cout << "Anna puhelinnumero: ";
 	getline(cin, puhelinnumero_);
 }
-
+/*------------------------------------------------
+*name: tulosta() const
+*action: print person information
+-------------------------------------------------*/
 void Henkilo::tulosta() const
 {
-	cout << etunimi_ << " " << sukunimi_ << " " << osoite_ << " " <<
-		puhelinnumero_ << " ";
+	cout << etunimi_ << " " << sukunimi_ << " " << osoite_ << " " << puhelinnumero_ << " ";
+}
+/*--------------------------------------------------
+*name: tallennaTiedotTyontekija() const
+*action: save workers information
+----------------------------------------------------*/
+void Henkilo::tallennaTiedotTyontekija() const {
+
+	ofstream Opettaja;
+	Opettaja.open("Opettaja.csv", ofstream::app);
+	Opettaja << etunimi_ << ";" << sukunimi_ << ";" << osoite_ << ";" << puhelinnumero_ << ";";
+}
+/*------------------------------------------------
+*name: tallennaTiedotOppilas() const
+*action: save student information
+-------------------------------------------------*/
+void Henkilo::tallennaTiedotOpiskelija() const
+{
+	ofstream Opiskelija;
+	Opiskelija.open("Opiskelija.csv", ofstream::app);
+	Opiskelija << etunimi_ << ";" << sukunimi_ << ";" << osoite_ << ";" << puhelinnumero_ << ";";
 }
--- Rekisteri/Reskisteri/Henkilo.h
+++ Rekisteri/Reskisteri/Henkilo.h
@@ -1,18 +1,20 @@
 #pragma once
 #include <string>
 using std::string;
+
 class Henkilo
 {
 public:
-	Henkilo();
-	Henkilo(string etunimi, string sukunimi, string osoite,
-		string puhelinnumero);
-	Henkilo(const Henkilo& alkup);
-	~Henkilo();
+	Henkilo(); //default constructor
+	Henkilo(string etunimi, string sukunimi, string osoite, string puhelinnumero); //parameter constructor
+	Henkilo(const Henkilo& alkup); //copy constructor
+	~Henkilo(); //destructor
+				//getter
 	string annaEtunimi() const;
 	string annaSukunimi() const;
 	string annaOsoite() const;
 	string annaPuhelinnumero() const;
+	//setter
 	void asetaEtunimi(string etunimi);
 	void asetaSukunimi(string sukunimi);
 	void asetaOsoite(string osoite);
@@ -20,10 +22,11 @@ public:
 
 	void kysyTiedot();
 	void tulosta() const;
+	void tallennaTiedotTyontekija() const;
+	void tallennaTiedotOpiskelija() const;
 private:
 	string etunimi_;
 	string sukunimi_;
 	string osoite_;
 	string puhelinnumero_;
 };
-
