fba8ba54fc9bc6f8d48d66de8887c3fd8955fea2
Minna Hannula <m.hannula@elisanet.fi>
2019-03-10 19:07:33 +0200

    Delete clock.tsx

--- Web UI/pages/clock.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '../src/components/NavBar';
-import Clock from '../src/components/Clock';
-
-export default () => (
-    <div>
-        <Navbar />
-        <Clock />
-    </div>
-);
