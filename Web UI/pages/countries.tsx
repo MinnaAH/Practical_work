3d83417215d61410c9d9bf3901bcf4104576defe
Minna Hannula <m.hannula@elisanet.fi>
2019-03-10 19:07:42 +0200

    Delete countries.tsx

--- Web UI/pages/countries.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '../src/components/NavBar';
-import Countries from '../src/components/Countries';
-
-export default () => (
-  <div>
-    <Navbar />
-    <Countries />
-  </div>
-);
